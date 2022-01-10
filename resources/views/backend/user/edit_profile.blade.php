3dbf26d8251315fe01bfae4a0fa841eb7b70a2ad
LIASY032 <66517361+LIASY032@users.noreply.github.com>
2022-01-10 13:41:19 +1030

    Update edit_profile.blade.php

---
+++ resources/views/backend/user/edit_profile.blade.php
@@ -0,0 +1,134 @@
+@extends('admin.admin_master')
+@section('admin')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <div class="content-wrapper">
+        <div class="container-full">
+
+            <!-- Basic Forms -->
+            <div class="box">
+                <div class="box-header with-border">
+                    <h4 class="box-title">Manage Profile</h4>
+
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col">
+                            <form method="POST" action="{{ route('users.update', $editData->id) }}">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <h5>User Name <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="text" name="name" value="{{ $editData->name }}"
+                                                            class="form-control" required />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <h5>User Email <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="email" name="email" class="form-control"
+                                                            value="{{ $editData->email }}" required />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <h5>User Mobile <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="text" name="mobile" class="form-control"
+                                                            value="{{ $editData->mobile }}" required />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <h5>User Address <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="text" name="address" class="form-control"
+                                                            value="{{ $editData->address }}" required />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <h5>Gender<span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <select name="gender" id="gender" required class="form-control">
+                                                            <option value="" selected="" disabled="">Select Gender</option>
+                                                            <option value="Male"
+                                                                {{ $editData->gender == 'Male' ? 'selected' : '' }}>
+                                                                Male</option>
+                                                            <option value="Female"
+                                                                {{ $editData->gender == 'Female' ? 'selected' : '' }}>
+                                                                Female
+                                                            </option>
+
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <h5>Profile Image<span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="file" name="image" class="form-control" id="image" />
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+
+                                                    <div class="controls">
+                                                        <img id="showImage"
+                                                            src="{{ !empty($editData->image) ? url('upload/user_images' . $editData->image) : asset('backend/images/user3-128x128.jpg') }}"
+                                                            style="width: 100px; border: 1px solid #000000" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+
+                                    </div>
+
+                                    <div class="text-xs-right">
+                                        <input type="submit" class="btn btn-rounded btn-info mb-5" value="Update"></input>
+                                    </div>
+                            </form>
+
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+
+
+        </div>
+    </div>
+    <script type="text/javascript">
+        $(document).ready(function() {
+
+
+            $('#image').change(function(e) {
+
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    $('#showImage').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(e.target.files[0]);
+            })
+        })
+    </script>
+@endsection
